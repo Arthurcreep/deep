b0b1f1ec90b6c774217cd53400fdf4d1428f56a5
Arthurcreep <112624448+Arthurcreep@users.noreply.github.com>
2024-12-08 17:49:14 +0300

    add WebSocket

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
-const {RestClientV5} = require('bybit-api')
+const {RestClientV5, WebsocketClient, DefaultLogger} = require('bybit-api')
 const { Bot } = require('grammy')
+const { WebSocket } = require('ws') 
   const useTestnet = false
   const client = new RestClientV5({
     key: process.env.API_KEY,
@@ -10,7 +11,7 @@ const { Bot } = require('grammy')
   )
 
   let arrTheSearch = {
-      max: '99000',
+      max: '100070',
       min: '80000'
   }
 const bot  = new Bot(process.env.API_BOT)
@@ -18,54 +19,49 @@ const bot  = new Bot(process.env.API_BOT)
 bot.api.setMyCommands([
   {command:'start', description:'Start the bot'},
   {command:'wallet', description:'Chekend the bot'},
+  {command: 'position', description:'data of position'}
 ])
 let strBuy = ''
 let strBtc = ''
-
-
-// Получаем значение текущего времени с сервера для синхронизации и запрашиваем значение BTC
-setInterval(() => {
-  let currentTime = 
-client
-  .getServerTime()
-  .then((response) => {
-    client
-    .getKline({
-      category: 'spot',
-      symbol: 'BTCUSDT',
-      interval: '1',
-      start: response.time,
-      end: response.time,
+let pos = 'Пусто ни чего не набрал'
+const url = "wss://stream.bybit.com/v5/public/spot"
+const connection =  new WebSocket(url)
+const logger = {
+  ...DefaultLogger,
+  silly: (...params) => console.log('silly', ...params),
+};
+const wsClient = new WebsocketClient(
+  {
+    market: 'v5',
+  },
+  logger,
+);
+// Функция покупки
+async function getOrder() {
+  client
+  .submitOrder({
+    category: 'spot',
+    symbol: '1INCHUSDT',
+    side: 'Buy',
+    orderType: 'Market',
+    qty: '420',
   })
   .then((response) => {
-    let priceBtcClose = response.result.list[0][4] 
-    strBtc = response.result.list[0][4]
-    if (Number(priceBtcClose) <= Number(arrTheSearch.min)) {
-      client
-      .submitOrder({
-        category: 'spot',
-        symbol: '1INCHUSDT',
-        side: 'Buy',
-        orderType: 'Market',
-        qty: '420'
-    })
-    .then((response) => {
-        console.log(response);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
-    }
-      console.log(priceBtcClose)
+    pos = response.result.orderId
+    console.log(response.result.orderId);
   })
-    .catch((error) => {
-        console.error(error);
-    })
-    return response.time
-    console.log(response.time)
-  })
-}, 5000)
-// Работа Бота Данные BTC при старте
+  .catch((error) => {
+    console.error(error);
+  });
+}
+wsClient.on('update', (data) => {
+  // Условие входа в сделку
+  if (Number(data.data[0].close) < arrTheSearch.min) {
+    getOrder()
+  }
+});
+const topics = 'kline.5.BTCUSDT';
+wsClient.subscribeV5(topics, 'spot');
 async function getBtcDate() {
   client
   .getServerTime()
@@ -103,5 +99,9 @@ async function getBalance() {
 getBalance()
 bot.command('wallet', async (ctx) => {
   await ctx.reply(strBuy + '_' + 'USDT')
+  let user = ctx.msg.from.id
+})
+bot.command('position', async (ctx) => {
+  await ctx.reply(`Новый оредер ${pos}`)
 })
 bot.start()
